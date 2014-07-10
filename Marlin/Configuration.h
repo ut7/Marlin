5e2685316c7e66f2592edca3a29c7678aaa9f29e
Jonathan Perret <jonathan@ut7.fr>
2014-07-10 16:35:19 +0200

    Set temperature sensor for E3Dv6

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -162,7 +162,7 @@
 // 110 is Pt100 with 1k pullup (non standard)
 // 70 is 500C thermistor for Pico hot end
 
-#define TEMP_SENSOR_0 0
+#define TEMP_SENSOR_0 5
 #define TEMP_SENSOR_1 0
 #define TEMP_SENSOR_2 0
 #define TEMP_SENSOR_BED 0
