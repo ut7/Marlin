e21043314f432332c9c637e98d3a91ae580f2c06
Jonathan Perret <jonathan@ut7.fr>
2014-07-14 18:06:15 +0200

    PID Autotune

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -217,9 +217,9 @@
 
 // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 // Ultimaker
-    #define  DEFAULT_Kp 22.2
-    #define  DEFAULT_Ki 1.08
-    #define  DEFAULT_Kd 114
+//    #define  DEFAULT_Kp 22.2
+//    #define  DEFAULT_Ki 1.08
+//    #define  DEFAULT_Kd 114
 
 // MakerGear
 //    #define  DEFAULT_Kp 7.0
@@ -230,6 +230,12 @@
 //    #define  DEFAULT_Kp 63.0
 //    #define  DEFAULT_Ki 2.25
 //    #define  DEFAULT_Kd 440
+
+// Auto-calibrated E3D V6
+    #define  DEFAULT_Kp 35.13
+    #define  DEFAULT_Ki 4.43
+    #define  DEFAULT_Kd 69.63
+
 #endif // PIDTEMP
 
 // Bed Temperature Control
